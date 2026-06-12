3b90d8e19db318771e4da981421c5864a5ab8499
fxruq <temiladefaruq2006@gmail.com>
2026-06-13 00:07:03 +0100

    finished

---
+++ reg-users/func.py
@@ -0,0 +1,16 @@
+
+from python_functions import validate_name, validate_email, validate_password, top_level_domains
+
+print("validate_name\n")
+print(validate_name.__doc__)
+print("--------------------\n")
+
+print("validate_email\n")
+print(validate_email.__doc__) 
+print("--------------------\n")
+
+print("validate_password\n")
+print(validate_password.__doc__)
+
+
+print(top_level_domains)
